c236f1e7350c88cbabab25c26d4608322c747d30
Kayla Wood <kaylaewood@gmail.com>
2020-01-09 10:37:19 -0700

    Add basic Router structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className='app'>
-
+      <Route exact path = '/'>
+        <p>/</p>
+      </Route>
+      <Route exact path = '/select'>
+        <p>/select</p>
+      </Route>
+      <Route exact path = '/play'>
+        <p>/play</p>
+      </Route>
+      <Route exact path = '/end'>
+        <p>/end</p>
+      </Route>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
