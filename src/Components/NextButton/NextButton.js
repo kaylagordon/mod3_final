65016eb00d832e914bbb4ff9b2726c78e37b5449
Kayla Wood <kaylaewood@gmail.com>
2020-01-09 09:13:03 -0700

    Add folders for HomePage and NextButton

---
+++ src/Components/NextButton/NextButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NextButton.scss';
+
+function NextButton() {
+  return (
+    <div className='nextButton'>
+
+    </div>
+  );
+}
+
+export default NextButton;
