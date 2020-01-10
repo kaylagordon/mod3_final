b6c2a8f17baeb3590c8680d271cfa7ead3c83410
Kayla Wood <kaylaewood@gmail.com>
2020-01-10 11:36:19 -0700

    Add selected time and problem set to Form state

--- src/Components/GameForm/GameForm.js
+++ src/Components/GameForm/GameForm.js
@@ -1,25 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './GameForm.scss';
 
-function GameForm() {
-  return (
-    <form>
-      <p className='form-label'>choose your topic:</p>
-      <div className='form-buttons-container'>
-        <button className='form-button' id='pemdas'>PEMDAS</button>
-        <button className='form-button' id='factoring'>FACTORING</button>
-        <button className='form-button' id='deriving'>DERIVING</button>
-        <button className='form-button' id='trigonometry'>TRIGONOMETRY</button>
-        <button className='form-button' id='mixed'>MIXED BAG</button>
-      </div>
-      <p className='form-label'>choose your time:</p>
-      <div className='form-buttons-container'>
-        <button className='form-button' id='min-3'>3 MIN</button>
-        <button className='form-button' id='min-1'>1 MIN</button>
-        <button className='form-button' id='sec-30'>30 SEC</button>
-      </div>
-    </form>
-  );
+class GameForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedProblemSet: '',
+      selectedTime: ''
+    }
+  }
+
+  updateSelected = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <p className='form-label'>choose your topic:</p>
+        <div className='form-buttons-container'>
+          <button
+            className='form-button'
+            value='pemdas'
+            name='selectedProblemSet'
+            onClick={this.updateSelected}
+          >PEMDAS</button>
+          <button
+            className='form-button'
+            value='factoring'
+            name='selectedProblemSet'
+            onClick={this.updateSelected}
+          >FACTORING</button>
+          <button
+            className='form-button'
+            value='deriving'
+            name='selectedProblemSet'
+            onClick={this.updateSelected}
+          >DERIVING</button>
+          <button
+            className='form-button'
+            value='trigonometry'
+            name='selectedProblemSet'
+            onClick={this.updateSelected}
+          >TRIGONOMETRY</button>
+          <button
+            className='form-button'
+            value='mixed'
+            name='selectedProblemSet'
+            onClick={this.updateSelected}
+          >MIXED BAG</button>
+        </div>
+        <p className='form-label'>choose your time:</p>
+        <div className='form-buttons-container'>
+          <button
+            className='form-button'
+            value='180'
+            name='selectedTime'
+            onClick={this.updateSelected}
+          >3 MIN</button>
+          <button
+            className='form-button'
+            value='60'
+            name='selectedTime'
+            onClick={this.updateSelected}
+          >1 MIN</button>
+          <button
+            className='form-button'
+            value='30'
+            name='selectedTime'
+            onClick={this.updateSelected}
+          >30 SEC</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default GameForm;
