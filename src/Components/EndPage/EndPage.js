bd28e5a678ec0c95bd5a07f03b9ea980684adeb9
Kayla Wood <kaylaewood@gmail.com>
2020-01-13 14:06:23 -0600

    Refactor EndPage for readbility

--- src/Components/EndPage/EndPage.js
+++ src/Components/EndPage/EndPage.js
@@ -3,17 +3,25 @@ import { connect } from 'react-redux';
 import './EndPage.scss';
 
 function EndPage({ gameStats, time }) {
+  const speed =
+    ((gameStats.numberCorrect
+    / time
+    * 60)
+    .toFixed(1))
+
+  const accuracy =
+    ((gameStats.numberCorrect /
+    (gameStats.numberCorrect + gameStats.numberIncorrect)
+    * 100)
+    .toFixed(1))
+
   return (
     <section className='endPage'>
       <h1>GREAT WORK!</h1>
       <h2>your results:</h2>
       <p className='end-data-text'><span className='highlight-data' id='number-correct-text'>{gameStats.numberCorrect}</span> correct answers</p>
-      <p className='end-data-text'><span className='highlight-data' id='number-correct--per-minute-text'>{
-        (gameStats.numberCorrect / time * 60).toFixed(1)
-      }</span> correct answers per minute</p>
-      <p className='end-data-text'><span className='highlight-data' id='percent-accurate-text'>{
-        `${(gameStats.numberCorrect / (gameStats.numberCorrect + gameStats.numberIncorrect) * 100).toFixed(1)}%`
-      }</span> accuracy</p>
+      <p className='end-data-text'><span className='highlight-data' id='number-correct--per-minute-text'>{speed}</span> correct answers per minute</p>
+      <p className='end-data-text'><span className='highlight-data' id='percent-accurate-text'>{accuracy}%</span> accuracy</p>
     </section>
   );
 }
