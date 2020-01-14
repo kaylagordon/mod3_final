5ea7923159f17fa65b010194f404b53fd1f65259
Kayla Wood <kaylaewood@gmail.com>
2020-01-14 10:27:50 -0700

    Add timer display

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { updateTimer } from '../../actions';
 
-export const Header = ({ startTime, updateTimer }) => {
-  const countdown = () => {
-    var minutes = Math.floor(startTime / 60);
-    var seconds = startTime % 60;
+export class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      minutesLeft: 0,
+      secondsLeft: 0
+    }
+  }
 
+  countdown = () => {
     const tick = () => {
-      seconds--;
-      if(seconds > 0) {
+      if(this.state.secondsLeft > 0) {
+        let newSeconds = this.state.secondsLeft - 1;
+        this.setState({
+          secondsLeft: newSeconds
+        })
         setTimeout(tick, 1000);
-      } else if (minutes > 1) {
-        minutes--;
-        seconds = 60;
-        tick()
+      } else if (this.state.minutesLeft >= 1) {
+        let newMinutes = this.state.minutesLeft - 1;
+        this.setState({
+          minutesLeft: newMinutes,
+          secondsLeft: 60
+        })
+        setTimeout(tick, 1000);
       } else {
-        updateTimer('isOver', true)
+        this.props.updateTimer('isOver', true)
       }
     }
 
     tick();
   }
 
-  return (
-    <header>
+  componentDidMount = async () => {
+    await this.setState({
+      minutesLeft: Math.floor(this.props.startTime / 60),
+      secondsLeft: this.props.startTime % 60
+    })
+
+    this.countdown();
+  }
+
+  render() {
+    return (
+      <header>
       <h1>MATH<span className='highlight-title'>4</span>YOU</h1>
-      <h2>TIME LEFT: {countdown()}</h2>
+      <h2>TIME LEFT: 0{this.state.minutesLeft}:{this.state.secondsLeft < 10 ? `0${this.state.secondsLeft}` : this.state.secondsLeft}</h2>
       <Link to = '/select'>
-        <button className='go-back-button'>GO BACK</button>
+      <button className='go-back-button'>GO BACK</button>
       </Link>
-    </header>
-  );
+      </header>
+    );
+  }
 };
 
 export const mapStateToProps = state => ({
