311db85defee3cd46be6ffc1d97e9154beeb6685
Kayla Wood <kaylaewood@gmail.com>
2020-01-16 10:26:22 -0700

    Add testing for MathCard

--- src/Components/MathCard/MathCard.test.js
+++ src/Components/MathCard/MathCard.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { MathCard, mapStateToProps, mapDispatchToProps } from './MathCard';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { increaseCorrect, increaseIncorrect} from '../../actions';
+import { getAnswer } from '../../apiCalls/apiCalls';
+
+jest.mock('../../apiCalls/apiCalls');
 
 describe('MathCard', () => {
   let wrapper;
@@ -20,6 +23,81 @@ describe('MathCard', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update answer in state', () => {
+    const mockEvent = {
+      target: {
+        value: 73
+      }
+    }
+
+    wrapper.instance().updateAnswer(mockEvent);
+
+    expect(wrapper.state('answer')).toEqual(73)
+
+  })
+
+  it('should invoke increaseCorrect when answer is correct', async () => {
+
+    wrapper = mount(<MathCard
+      currentProblemSet={mockCurrentProblemSet}
+      increaseCorrect={mockIncreaseCorrect}
+      increaseIncorrect={mockIncreaseIncorrect}
+    />);
+
+    wrapper.setState({
+      answer: '2x-x'
+    });
+
+    const mockData = {
+      result: '2 x - x'
+    };
+
+    getAnswer.mockImplementation(() => {
+      return Promise.resolve(mockData);
+    });
+
+    await wrapper.instance().checkAnswer();
+
+    expect(mockIncreaseCorrect).toHaveBeenCalled();
+  })
+
+  it('should invoke increaseIncorrect when answer is correct', async () => {
+
+    wrapper = mount(<MathCard
+      currentProblemSet={mockCurrentProblemSet}
+      increaseCorrect={mockIncreaseCorrect}
+      increaseIncorrect={mockIncreaseIncorrect}
+    />);
+
+    wrapper.setState({
+      answer: '2-x'
+    });
+
+    const mockData = {
+      result: '2 x - x'
+    };
+
+    getAnswer.mockImplementation(() => {
+      return Promise.resolve(mockData);
+    });
+
+    await wrapper.instance().checkAnswer();
+
+    expect(mockIncreaseIncorrect).toHaveBeenCalled();
+  })
+
+  it('should make a new card', () => {
+    wrapper.setState({
+      expression: '2+2',
+      answer: '4',
+      evaluatedTo: 'correct'
+    })
+
+    wrapper.instance().getNewCard();
+
+    expect(wrapper.state('answer')).toEqual('')
+  })
 
   describe('mapStateToProps', () => {
     it('should return an object with game information', () => {
